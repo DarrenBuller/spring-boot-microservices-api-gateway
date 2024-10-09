b571ddce9cf237fddbdef061941d30ede960a4ac
Darren Buller <stash-taco-remake@duck.com>
2024-10-09 10:39:19 +0100

    Add initial Kafka and switch to host networking

--- src/main/java/com/example/gateway/routes/Routes.java
+++ src/main/java/com/example/gateway/routes/Routes.java
@@ -21,7 +21,7 @@ public class Routes {
         @Bean
         public RouterFunction<ServerResponse> productServiceRoute() {
                 return route("product_service")
-                                .route(RequestPredicates.path("/api/product"), http("http://product:8080"))
+                                .route(RequestPredicates.path("/api/product"), http("http://localhost:8080"))
                                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
                                                 URI.create("forward:/fallbackRoute")))
                                 .build();
@@ -30,7 +30,7 @@ public class Routes {
         @Bean
         public RouterFunction<ServerResponse> orderServiceRoute() {
                 return route("order_service")
-                                .route(RequestPredicates.path("/api/order"), http("http://order:8081"))
+                                .route(RequestPredicates.path("/api/order"), http("http://localhost:8081"))
                                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
                                                 URI.create("forward:/fallbackRoute")))
                                 .build();
@@ -39,7 +39,7 @@ public class Routes {
         @Bean
         public RouterFunction<ServerResponse> inventoryServiceRoute() {
                 return route("inventory_service")
-                                .route(RequestPredicates.path("/api/inventory"), http("http://inventory:8082"))
+                                .route(RequestPredicates.path("/api/inventory"), http("http://localhost:8082"))
                                 .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
                                                 URI.create("forward:/fallbackRoute")))
                                 .build();
@@ -49,7 +49,7 @@ public class Routes {
         public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
                 return route("product_service_swagger")
                                 .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
-                                                HandlerFunctions.http("http://product:8080"))
+                                                HandlerFunctions.http("http://localhost:8080"))
                                 .filter(setPath("/api-docs"))
                                 .build();
         }
@@ -58,7 +58,7 @@ public class Routes {
         public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
                 return route("order_service_swagger")
                                 .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"),
-                                                HandlerFunctions.http("http://order:8081"))
+                                                HandlerFunctions.http("http://localhost:8081"))
                                 .filter(setPath("/api-docs"))
                                 .build();
         }
